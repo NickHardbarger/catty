a79c669405e7f8564650269ca8b43295fb0f43ef
NickHardbarger <nickhardbarger13@gmail.com>
2025-05-23 22:36:23 -0400

    added dev shell

--- flake.nix
+++ flake.nix
@@ -1,15 +1,21 @@
 {
-  description = "A very basic flake";
+  description = "catty development flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        packages = with pkgs; [
+          gnumake
+          vte
+        ];
+      };
+    };
 }
